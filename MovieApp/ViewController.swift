dad09512b96ddf3bc378c8ede1ee4ed1f53b4dab
InbeomPyo <>
2024-03-29 18:33:26 +0900

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by 60157143 on 2024/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
